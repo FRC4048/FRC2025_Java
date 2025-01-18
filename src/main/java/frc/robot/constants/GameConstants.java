d1899df90cce5833fd76310c2a905fb2038a06f8
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-18 18:02:46 -0500

    Added constants values

---
+++ src/main/java/frc/robot/constants/GameConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.constants;
+
+public class GameConstants {
+    public static final double MAX_AUTO_ALIGN_SPEED = 0.9;
+    public static final boolean SWERVE_DEBUG = true; // TODO: change
+}
