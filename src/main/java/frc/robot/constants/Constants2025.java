d7ccdfcc9841090cac2b12f20ac29c3c651bdd52
codetoad <softwareenginer@pm.me>
2025-01-19 14:35:39 -0500

    fixed merge conflict

--- src/main/java/frc/robot/constants/Constants2025.java
+++ src/main/java/frc/robot/constants/Constants2025.java
@@ -1,22 +1,11 @@
 package frc.robot.constants;
 
-<<<<<<< HEAD
-public class Constants2025{
+public class Constants2025 {
     public static final int ELEVATOR_MOTOR_1_ID = 0; //TODO: change later
     public static final double INITIAL_ELEVATOR_HEIGHT = 0; //TODO: change later
     public static final int ELEVATOR_MOTOR_2_ID = 1; //TODO: change later
     public static final int SHOOTER_MOTOR_1_ID = 0; //TODO: change later
     public static final int SHOOTER_MOTOR_2_ID = 1; //TODO: change later
     public static final int SHOOTER_TILT_MOTOR_ID = 2; //TODO: change later
-    public static final Mode ALGAE_ROLLER_CAN_ID = 0; //TODO: change later
-
-=======
-public class Constants2025 {
-  public static final int ELEVATOR_MOTOR_1_ID = 0; // TODO: change later
-  public static final double INITIAL_ELEVATOR_HEIGHT = 0; // TODO: change later
-  public static final int ELEVATOR_MOTOR_2_ID = 1; // TODO: change later
-  public static final int SHOOTER_MOTOR_1_ID = 0; // TODO: change later
-  public static final int SHOOTER_MOTOR_2_ID = 1; // TODO: change later
-  public static final int SHOOTER_TILT_MOTOR_ID = 2; // TODO: change later
->>>>>>> 5987520b87777523b2b9a26acb64d447f38b0f8b
+    public static final int ALGAE_ROLLER_CAN_ID = 0; //TODO: change later
 }
