4e3a5711404c1c9a515a421ff23126a9b520aa10
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-19 12:58:21 -0500

    fixed pr comments?

--- src/main/java/frc/robot/utils/Apriltag.java
+++ src/main/java/frc/robot/utils/Apriltag.java
@@ -1,27 +1,33 @@
 package frc.robot.utils;
 
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
 // import org.jetbrains.annotations.Nullable;
 
 /** Values are for 2024 game. They will need to change but serve as an example */
 public enum Apriltag {
-  ONE(Units.inchesToMeters(593.68), Units.inchesToMeters(9.68), Units.inchesToMeters(53.38)),
-  TWO(Units.inchesToMeters(637.21), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38)),
-  THREE(Units.inchesToMeters(652.73), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13)),
-  FOUR(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13)),
-  FIVE(Units.inchesToMeters(578.77), Units.inchesToMeters(323.00), Units.inchesToMeters(53.38)),
-  SIX(Units.inchesToMeters(72.5), Units.inchesToMeters(323.00), Units.inchesToMeters(53.38)),
-  SEVEN(Units.inchesToMeters(-1.50), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13)),
-  EIGHT(Units.inchesToMeters(-1.50), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13)),
-  NINE(Units.inchesToMeters(14.02), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38)),
-  TEN(Units.inchesToMeters(57.54), Units.inchesToMeters(9.68), Units.inchesToMeters(53.38)),
-  ELEVEN(Units.inchesToMeters(468.69), Units.inchesToMeters(146.19), Units.inchesToMeters(52.00)),
-  TWELVE(Units.inchesToMeters(468.69), Units.inchesToMeters(177.10), Units.inchesToMeters(52.00)),
-  THIRTEEN(Units.inchesToMeters(441.74), Units.inchesToMeters(161.62), Units.inchesToMeters(52.00)),
-  FOURTEEN(Units.inchesToMeters(209.48), Units.inchesToMeters(161.62), Units.inchesToMeters(52.00)),
-  FIFTEEN(Units.inchesToMeters(182.73), Units.inchesToMeters(177.10), Units.inchesToMeters(52.00)),
-  SIXTEEN(Units.inchesToMeters(182.73), Units.inchesToMeters(146.19), Units.inchesToMeters(52.00));
+  // ONE(Units.inchesToMeters(593.68), Units.inchesToMeters(9.68), Units.inchesToMeters(53.38)),
+  // TWO(Units.inchesToMeters(637.21), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38)),
+  // THREE(Units.inchesToMeters(652.73), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13)),
+  // FOUR(Units.inchesToMeters(652.73), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13)),
+  // FIVE(Units.inchesToMeters(578.77), Units.inchesToMeters(323.00), Units.inchesToMeters(53.38)),
+  // SIX(Units.inchesToMeters(72.5), Units.inchesToMeters(323.00), Units.inchesToMeters(53.38)),
+  // SEVEN(Units.inchesToMeters(-1.50), Units.inchesToMeters(218.42), Units.inchesToMeters(57.13)),
+  // EIGHT(Units.inchesToMeters(-1.50), Units.inchesToMeters(196.17), Units.inchesToMeters(57.13)),
+  // NINE(Units.inchesToMeters(14.02), Units.inchesToMeters(34.79), Units.inchesToMeters(53.38)),
+  // TEN(Units.inchesToMeters(57.54), Units.inchesToMeters(9.68), Units.inchesToMeters(53.38)),
+  // ELEVEN(Units.inchesToMeters(468.69), Units.inchesToMeters(146.19),
+  // Units.inchesToMeters(52.00)),
+  // TWELVE(Units.inchesToMeters(468.69), Units.inchesToMeters(177.10),
+  // Units.inchesToMeters(52.00)),
+  // THIRTEEN(Units.inchesToMeters(441.74), Units.inchesToMeters(161.62),
+  // Units.inchesToMeters(52.00)),
+  // FOURTEEN(Units.inchesToMeters(209.48), Units.inchesToMeters(161.62),
+  // Units.inchesToMeters(52.00)),
+  // FIFTEEN(Units.inchesToMeters(182.73), Units.inchesToMeters(177.10),
+  // Units.inchesToMeters(52.00)),
+  // SIXTEEN(Units.inchesToMeters(182.73), Units.inchesToMeters(146.19),
+  // Units.inchesToMeters(52.00));
+  PLACEHOLDER(0, 0, 0); // Placeholder
 
   private final Translation3d translation;
 
