91f3364f232daeaade531ab9bee640f87cf60cb6
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-18 21:03:37 -0500

    added vendordeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2025_Java";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "e675efca4e27b286e3799d8cb9072bca9049efab";
-  public static final String GIT_DATE = "2025-01-13 20:22:18 EST";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "7ed79bbd6be46b44f7e034c813f64ed496994f3a";
+  public static final String GIT_DATE = "2025-01-17 22:44:39 EST";
   public static final String GIT_BRANCH = "LS_PathPlanner_3";
-  public static final String BUILD_DATE = "2025-01-16 18:21:01 EST";
-  public static final long BUILD_UNIX_TIME = 1737069661088L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-01-18 21:03:15 EST";
+  public static final long BUILD_UNIX_TIME = 1737252195763L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
