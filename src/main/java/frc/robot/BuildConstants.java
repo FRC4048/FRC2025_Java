0a566b8e881271e1104e2fa033f4b08f495102ab
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-25 19:28:16 -0500

    build

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2025_Java";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 35;
-  public static final String GIT_SHA = "286633ee8e6a389b1595de5464e527b010cc7774";
-  public static final String GIT_DATE = "2025-01-19 18:35:43 EST";
+  public static final int GIT_REVISION = 40;
+  public static final String GIT_SHA = "3b55522f0f4ebff2bdd51b47f822e9e962b3e10f";
+  public static final String GIT_DATE = "2025-01-25 19:27:49 EST";
   public static final String GIT_BRANCH = "LS_PathPlanner_3";
-  public static final String BUILD_DATE = "2025-01-19 18:35:53 EST";
-  public static final long BUILD_UNIX_TIME = 1737329753884L;
+  public static final String BUILD_DATE = "2025-01-25 19:28:00 EST";
+  public static final long BUILD_UNIX_TIME = 1737851280669L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
