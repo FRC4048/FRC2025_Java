bdb81409e5041c4b93acdef2a8a8464ab03b33ee
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-25 20:02:27 -0500

    fixed pr comments

--- src/main/java/frc/robot/subsystems/swervev3/bags/VisionMeasurement.java
+++ src/main/java/frc/robot/subsystems/swervev3/bags/VisionMeasurement.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems.swervev3.bags;
 import edu.wpi.first.math.geometry.Pose2d;
 
 /**
- * @param measurement estimated robot position calculated from apriltag
- * @param tag what tag produced the position measurement
- * @param timeOfMeasurement difference between the time the measurement was received by the robot
+ * measurement estimated robot position calculated from apriltag
+ * tag what tag produced the position measurement
+ * timeOfMeasurement difference between the time the measurement was received by the robot
  *     program and when it was sent over the network
  */
 public record VisionMeasurement(Pose2d measurement, double timeOfMeasurement) {}
