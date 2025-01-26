a7a8f0e6ee91837bb464ee35a5f19f6f45a991f7
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-26 12:52:27 -0500

    fixed errors, added tilting

--- src/main/java/frc/robot/commands/Intake/StartIntake.java
+++ src/main/java/frc/robot/commands/Intake/StartIntake.java
@@ -1,39 +1,41 @@
-package frc.robot.commands.intake;
+package frc.robot.commands.Intake;
 
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.Constants;
-import frc.robot.subsystems.intake.Intake;
-import frc.robot.utils.loggingv2.LoggableCommand;
+import frc.robot.subsystems.coral.CoralSubsystem;
+import frc.robot.utils.logging.LoggableCommand;
 
 public class StartIntake extends LoggableCommand {
-    private final Intake intake;
-    private final Timer timer = new Timer();
-    private final double motorRunTime; // temporary until  done testing
+  private final CoralSubsystem intake;
+  private final Timer timer = new Timer();
+  private final double motorRunTime; // temporary until  done testing
 
-    public StartIntake(Intake intake, double motorRunTime ) {
-        addRequirements(intake);
-        this.intake = intake;
-        this.motorRunTime = motorRunTime;
-    }
+  public StartIntake(CoralSubsystem intake, double motorRunTime) {
+    addRequirements(intake);
+    this.intake = intake;
+    this.motorRunTime = motorRunTime;
+  }
 
-    @Override
-    public void initialize() {
-        timer.reset();
-        timer.start();
-    }
-  
-    @Override
-    public void execute() {
-        intake.setMotorSpeed(Constants.INTAKE_MOTOR_1_SPEED, Constants.INTAKE_MOTOR_2_SPEED);
-    }
+  @Override
+  public void initialize() {
+    timer.reset();
+    timer.start();
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-        intake.stopMotors();
-    }
+  @Override
+  public void execute() {
+    intake.setShooterSpeed(Constants.INTAKE_MOTOR_SPEED);
+    intake.setAngle(Constants.INTAKE_TILT_SPEED);
+  }
 
-    @Override
-    public boolean isFinished() {
-        return timer.hasElapsed(motorRunTime);
-    }
+  @Override
+  public void end(boolean interrupted) {
+    intake.stopShooterMotors();
+    intake.stopTiltMotors();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(motorRunTime);
+  }
 }
--- src/main/java/frc/robot/commands/Intake/StopIntake.java
+++ src/main/java/frc/robot/commands/Intake/StopIntake.java
@@ -1,31 +1,29 @@
-package frc.robot.commands.intake;
+package frc.robot.commands.Intake;
 
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.utils.loggingv2.LoggableCommand;
 
 public class StopIntake extends LoggableCommand {
-    private final Intake intake;
+  private final Intake intake;
 
-    public StopIntake(Intake intake) {
-        addRequirements(intake);
-        this.intake = intake;
-    }
+  public StopIntake(Intake intake) {
+    addRequirements(intake);
+    this.intake = intake;
+  }
 
-    @Override
-    public void initialize() {
-    }
+  @Override
+  public void initialize() {}
 
-    @Override
-    public void execute() {
-        intake.stopMotors();
-    }
+  @Override
+  public void execute() {
+    intake.stopMotors();
+  }
 
-    @Override
-    public void end(boolean interrupted) {
-    }
+  @Override
+  public void end(boolean interrupted) {}
 
-    @Override
-    public boolean isFinished() {
-        return true;
-    }
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
 }
