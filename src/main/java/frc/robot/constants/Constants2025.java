9a6c042086d64662e5a02ad3a76e7962f6ebf65e
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-26 11:37:18 -0500

    removed un-needed

--- src/main/java/frc/robot/constants/Constants2025.java
+++ src/main/java/frc/robot/constants/Constants2025.java
@@ -6,7 +6,6 @@ public class Constants2025 extends GameConstants {
 
   // Elevator
   public static final int ELEVATOR_MOTOR_1_ID = 0; // TODO: change later
-  public static final double INITIAL_ELEVATOR_HEIGHT = 0; // TODO: change later
   public static final int ELEVATOR_MOTOR_2_ID = 1; // TODO: change later
 
   // Shooter
