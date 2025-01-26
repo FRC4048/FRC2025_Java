2b20c5f8da5ba1f5a413e61835a6bfa945127e7a
JAMthepersonj <wessjackson93@gmail.com>
2025-01-26 12:23:01 -0500

    removed constant from game constants

--- src/main/java/frc/robot/constants/GameConstants.java
+++ src/main/java/frc/robot/constants/GameConstants.java
@@ -6,5 +6,5 @@ public class GameConstants {
   public static final double GRAVITY = -9.81;
   public static final int LEFT_JOYSTICK_ID = 0;
   public static final int RIGHT_JOYSTICK_ID = 1;
-  public static final double ELEVATOR_TIMEOUT = 10; // TODO: change later
+
 }
