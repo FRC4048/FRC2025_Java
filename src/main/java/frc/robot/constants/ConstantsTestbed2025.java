b9ffbcc6f82138898b9e344cd8d1f1037b376d26
bengold22 <98672229+bengold22@users.noreply.github.com>
2025-02-01 12:17:00 -0500

    Tweaking testbed constants

--- src/main/java/frc/robot/constants/ConstantsTestbed2025.java
+++ src/main/java/frc/robot/constants/ConstantsTestbed2025.java
@@ -1,10 +1,7 @@
 package frc.robot.constants;
 
-public class ConstantsTestbedClean2025 extends Constants2025 {
-  /**
-   * This an example of overriding a specific motor ID for testing, no changes
-   * to this file should be committed to Git.
-   * 
+public class ConstantsTestbed2025 extends Constants2025 {
+  /**    
    * To use the TestBed constants, change Constants.java to read:
    *   public class Constants extends ConstantsTestbedClean2025
    * 
@@ -14,5 +11,10 @@ public class ConstantsTestbedClean2025 extends Constants2025 {
    * Then revert changes to Constants.java and ConstantsTestBed.java before committing.
    * 
    */
+
+   /**
+    * This an example of overriding a specific motor ID for testing, no changes
+    * to this file should be committed to Git.
+    */
   public static final int ALGAE_EXTENDER_MOTOR_ID = 6;
 }
