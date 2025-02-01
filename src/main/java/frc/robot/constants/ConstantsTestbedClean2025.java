9480dd2de3047efec5b56437070a46d661ed81f9
bengold22 <98672229+bengold22@users.noreply.github.com>
2025-02-01 12:03:04 -0500

    Updating instruments in TestBed file

--- src/main/java/frc/robot/constants/ConstantsTestbedClean2025.java
+++ src/main/java/frc/robot/constants/ConstantsTestbedClean2025.java
@@ -1,5 +1,18 @@
 package frc.robot.constants;
 
 public class ConstantsTestbedClean2025 extends Constants2025 {
+  /**
+   * This an example of overriding a specific motor ID for testing, no changes
+   * to this file should be committed to Git.
+   * 
+   * To use the TestBed constants, change Constants.java to read:
+   *   public class Constants extends ConstantsTestbedClean2025
+   * 
+   * instead of:
+   *   public class Constants extends Constants2025
+   * 
+   * Then revert changes to Constants.java and ConstantsTestBed.java before committing.
+   * 
+   */
   public static final int ALGAE_EXTENDER_MOTOR_ID = 6;
 }
