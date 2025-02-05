c76814eec3aac10365674fa92b25b0573f1121ea
Code Toad <55628278+goatfanboi23@users.noreply.github.com>
2025-02-05 18:32:27 -0500

    reset gyro on init (#115)
    Co-authored-by: codetoad <softwareenginer@pm.me>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,8 @@ import com.pathplanner.lib.pathfinding.Pathfinding;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.drivetrain.SetInitOdom;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.drivetrain.ResetGyro;
 import frc.robot.commands.drivetrain.WheelAlign;
 import frc.robot.constants.Constants;
 import frc.robot.utils.RobotMode;
@@ -85,9 +87,19 @@ public class Robot extends LoggedRobot {
       CommandLogger.get().log();
     }
     if (counter == 0) {
-      new WheelAlign(m_robotContainer.getDrivetrain()).schedule();
+      actualInit();
     }
     counter++;
+  }
+
+  /**
+   * Use this instead of robot init.
+   */
+  private void actualInit() {
+    new SequentialCommandGroup(
+            new WheelAlign(m_robotContainer.getDrivetrain()),
+            new ResetGyro(m_robotContainer.getDrivetrain())
+    ).schedule();
   }
 
   @Override
