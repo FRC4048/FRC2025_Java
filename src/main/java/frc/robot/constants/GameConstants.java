64f27db31f6d492384c6bb4c0c5ead6eef04e592
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-02-05 20:04:55 -0500

    afdasf

--- src/main/java/frc/robot/constants/GameConstants.java
+++ src/main/java/frc/robot/constants/GameConstants.java
@@ -37,7 +37,7 @@ public class GameConstants {
   public static final int ROLL_ALGAE_TIMEOUT = 10;
   public static final double HIHI_EXTENDER_TIMEOUT = 10;
   public static final double HIHI_ROLLER_TIMEOUT = 5;
-  public static final int INTAKE_CORAL_TIMEOUT = 10;  
+  public static final int INTAKE_CORAL_TIMEOUT = 10;
 
   // Logging
   public static final long MAX_LOG_TIME_WAIT = 10;
