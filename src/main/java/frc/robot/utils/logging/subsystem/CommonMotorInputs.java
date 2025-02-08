6f09c42d659d61f44ec86e62c6945437cf99dc76
codetoad <softwareenginer@pm.me>
2025-02-08 11:28:39 -0500

    improved CommonMotorInputs

--- src/main/java/frc/robot/utils/logging/subsystem/CommonMotorInputs.java
+++ src/main/java/frc/robot/utils/logging/subsystem/CommonMotorInputs.java
@@ -1,19 +1,18 @@
 package frc.robot.utils.logging.subsystem;
 
-import frc.robot.utils.logging.subsystem.builders.BuildableFolderMotorInputs;
 import frc.robot.utils.logging.subsystem.builders.MotorInputBuilder;
 
 public class CommonMotorInputs {
 
-  public static <R> MotorInputBuilder<R> hasEncoder(MotorInputBuilder<R> builder) {
-    return builder.encoderPosition().encoderVelocity();
+  public static <R> void hasEncoder(MotorInputBuilder<R> builder) {
+    builder.encoderPosition().encoderVelocity();
   }
 
-  public static <R> MotorInputBuilder<R> hasStatus(MotorInputBuilder<R> builder) {
-    return builder.motorCurrent().motorTemperature();
+  public static <R> void hasStatus(MotorInputBuilder<R> builder) {
+    builder.motorCurrent().motorTemperature();
   }
 
-  public static <R> BuildableFolderMotorInputs<R> hasLimits(MotorInputBuilder<R> builder) {
-    return builder.fwdLimit().revLimit().build();
+  public static <R> void hasLimits(MotorInputBuilder<R> builder) {
+    builder.fwdLimit().revLimit().build();
   }
 }
