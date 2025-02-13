195d259bebee55d573007051f45da63d99a395ea
Joseph Kantrowitz <63410586+Mobby42@users.noreply.github.com>
2025-02-12 20:46:05 -0500

    Created new spin byebye roller base commnd (#127)
    * Created new spin byebye roller base commnd
    
    * Changed timer system
    
    * removed tiemout counter
    
    * restart before set speed
    
    ---------
    
    Co-authored-by: codetoad <softwareenginer@pm.me>

---
+++ src/main/java/frc/robot/commands/byebye/SpinByeByeRoller.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.byebye;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.algaebyebyeroller.AlgaeByeByeRollerSubsystem;
+import frc.robot.utils.logging.LoggableCommand;
+import frc.robot.utils.logging.TimeoutLogger;
+import frc.robot.constants.Constants;
+
+public class SpinByeByeRoller extends LoggableCommand {
+  private final AlgaeByeByeRollerSubsystem byebyeRoller;
+  private final Timer timer;
+
+  
+  public SpinByeByeRoller(AlgaeByeByeRollerSubsystem byebyeRoller) {
+    timer = new Timer();
+    this.byebyeRoller = byebyeRoller;
+    addRequirements(byebyeRoller);
+  }
+
+  @Override
+  public void initialize() {
+    timer.restart();
+    byebyeRoller.setSpeed(Constants.BYEBYE_ROLLER_SPEED);
+  }
+
+  @Override
+  public void execute() {}
+
+  @Override
+  public void end(boolean interrupted) {
+    byebyeRoller.stopMotors();
+  }
+
+  @Override
+  public boolean isFinished() {
+    if (timer.hasElapsed(Constants.BYEBYE_SPIN_ROLLER_TIMEOUT)) {
+      return true;
+    }
+  return false;
+  }
+}
