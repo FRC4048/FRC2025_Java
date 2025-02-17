858ffa174573c688e8c1b91a8749c73420c06a3e
jamthepersonj <wessjackson93@gmail.com>
2025-02-17 12:51:34 -0500

    lint

--- src/main/java/frc/robot/subsystems/coral/MockCoralIOFollower.java
+++ src/main/java/frc/robot/subsystems/coral/MockCoralIOFollower.java
@@ -4,7 +4,7 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import frc.robot.utils.logging.subsystem.inputs.MotorInputs;
 
 public class MockCoralIOFollower implements CoralIOFollower {
-  
+
   @Override
   public void updateInputs(MotorInputs inputs) {}
 
