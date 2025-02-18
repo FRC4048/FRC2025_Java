0d192b6297b71419edd739c294303e3ca099c5c4
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-02-18 11:47:38 -0500

    Added basic Diags for ByeBye

--- src/main/java/frc/robot/utils/diags/ByeByeDiag.java
+++ src/main/java/frc/robot/utils/diags/ByeByeDiag.java
@@ -1,21 +1,71 @@
 package frc.robot.utils.diags;
 
 import edu.wpi.first.networktables.GenericEntry;
-import frc.robot.subsystems.algaebyebyeroller.AlgaeByeByeRollerSubsystem;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.subsystems.algaebyebyetilt.AlgaeByeByeTiltSubsystem;
-import frc.robot.utils.diags.Diagnosable;
 
-public class ByeByeDiag implements Diagnosable{
-    private String title;
-    private String name;
-    private GenericEntry networkTableEntry;
-    private final AlgaeByeByeRollerSubsystem algaeByeByeRollerSubsystem;
-    private final AlgaeByeByeTiltSubsystem algaeByeByeTiltSubsystem;
-    public ByeByeDiag(String title, String name, AlgaeByeByeRollerSubsystem algaeByeByeRollerSubsystem, AlgaeByeByeTiltSubsystem algaeByeByeTiltSubsystem) {
-        this.title = title;
-        this.name = name;
-        this.algaeByeByeRollerSubsystem = algaeByeByeRollerSubsystem;
-        this.algaeByeByeTiltSubsystem = algaeByeByeTiltSubsystem;
-        reset();
+public class ByeByeDiag implements Diagnosable {
+  private String title;
+  private String nameRev;
+  private String nameFwd;
+  private boolean retracted;
+  private boolean extended;
+  private GenericEntry networkTableEntryRev;
+  private GenericEntry networkTableEntryFwd;
+  private final AlgaeByeByeTiltSubsystem algaeByeByeTiltSubsystem;
+
+  public ByeByeDiag(
+      String title,
+      String nameRev,
+      String nameFwd,
+      AlgaeByeByeTiltSubsystem algaeByeByeTiltSubsystem) {
+    this.title = title;
+    this.nameRev = nameRev;
+    this.nameFwd = nameFwd;
+    this.algaeByeByeTiltSubsystem = algaeByeByeTiltSubsystem;
+    reset();
+  }
+
+  @Override
+  public void reset() {
+    retracted = true;
+    extended = false;
+  }
+
+  @Override
+  public void refresh() {
+    if (networkTableEntryRev != null) {
+      retracted = getDiagRevResult();
+      networkTableEntryRev.setBoolean(retracted);
     }
-}   
+    if (networkTableEntryFwd != null) {
+      extended = getDiagFwdResult();
+      networkTableEntryFwd.setBoolean(extended);
+    }
+  }
+
+  boolean getDiagRevResult() {
+    return algaeByeByeTiltSubsystem.getReverseSwitchState();
+  }
+
+  boolean getDiagFwdResult() {
+    return algaeByeByeTiltSubsystem.getForwardSwitchState();
+  }
+
+  @Override
+  public void setShuffleBoardTab(ShuffleboardTab shuffleboardTab, int width, int height) {
+    networkTableEntryRev =
+        shuffleboardTab
+            .getLayout(title, BuiltInLayouts.kList)
+            .withSize(width, height)
+            .add(nameRev, true)
+            .getEntry();
+    networkTableEntryFwd =
+        shuffleboardTab
+            .getLayout(title, BuiltInLayouts.kList)
+            .withSize(width, height)
+            .add(nameFwd, false)
+            .getEntry();
+  }
+}
--- src/main/java/frc/robot/utils/diags/DiagBoolean.java
+++ src/main/java/frc/robot/utils/diags/DiagBoolean.java
@@ -5,60 +5,64 @@ import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 /**
- * Abstract diagnostics class for boolean sensors using DigitalInput
- * The diagnostics will turn green once the switch has changes to reflect both positions: ON and OFF
+ * Abstract diagnostics class for boolean sensors using DigitalInput The diagnostics will turn green
+ * once the switch has changes to reflect both positions: ON and OFF
  */
 public abstract class DiagBoolean implements Diagnosable {
 
-    private String title;
-    private String name;
-    private GenericEntry networkTableEntry;
+  private String title;
+  private String name;
+  private GenericEntry networkTableEntry;
 
-    private boolean seenFalse;
-    private boolean seenTrue;
+  private boolean seenFalse;
+  private boolean seenTrue;
 
-    /**
-     * Constructor
-     *
-     * @param name            the name of the unit. Will be used on the Shuffleboard
-     */
-    public DiagBoolean(String title, String name) {
-        this.title = title;
-        this.name = name;
+  /**
+   * Constructor
+   *
+   * @param name the name of the unit. Will be used on the Shuffleboard
+   */
+  public DiagBoolean(String title, String name) {
+    this.title = title;
+    this.name = name;
 
-        reset();
+    reset();
+  }
+
+  @Override
+  public void setShuffleBoardTab(ShuffleboardTab shuffleBoardTab, int width, int height) {
+    networkTableEntry =
+        shuffleBoardTab
+            .getLayout(title, BuiltInLayouts.kList)
+            .withSize(width, height)
+            .add(name, false)
+            .getEntry();
+  }
+
+  @Override
+  public void refresh() {
+    if (networkTableEntry != null) {
+      networkTableEntry.setBoolean(getDiagResult());
+    }
+  }
+
+  @Override
+  public void reset() {
+    seenFalse = seenTrue = false;
+  }
+
+  protected abstract boolean getValue();
+
+  // Package protected
+  boolean getDiagResult() {
+    boolean currentValue = getValue();
+    // Set the value for the state - whether the switch is pressed or not
+    if (currentValue) {
+      seenTrue = true;
+    } else {
+      seenFalse = true;
     }
 
-    @Override
-    public void setShuffleBoardTab(ShuffleboardTab shuffleBoardTab, int width, int height) {
-        networkTableEntry = shuffleBoardTab.getLayout(title, BuiltInLayouts.kList).withSize(width, height).add(name, false).getEntry();
-    }
-
-    @Override
-    public void refresh() {
-        if (networkTableEntry != null) {
-            networkTableEntry.setBoolean(getDiagResult());
-        }
-    }
-
-    @Override
-    public void reset() {
-        seenFalse = seenTrue = false;
-    }
-
-    protected abstract boolean getValue();
-
-    // Package protected
-    boolean getDiagResult() {
-        boolean currentValue = getValue();
-        // Set the value for the state - whether the switch is pressed or not
-        if (currentValue) {
-            seenTrue = true;
-        } else {
-            seenFalse = true;
-        }
-
-        return seenTrue && seenFalse;
-    }
-
+    return seenTrue && seenFalse;
+  }
 }
