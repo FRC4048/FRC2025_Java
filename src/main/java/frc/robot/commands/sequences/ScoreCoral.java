23b6678e32854e9e7ae194edb07611619d72aa28
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-02-18 14:52:44 -0500

    fixed pr

--- src/main/java/frc/robot/commands/sequences/ScoreCoral.java
+++ src/main/java/frc/robot/commands/sequences/ScoreCoral.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.sequential;
+package frc.robot.commands.sequences;
 
 import frc.robot.commands.elevator.SetElevatorStoredPosition;
 import frc.robot.constants.Constants;
@@ -8,8 +8,8 @@ import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
 import frc.robot.commands.coral.ShootCoral;
 
-public class SeqShootCoral extends LoggableSequentialCommandGroup {
-  public SeqShootCoral(CoralSubsystem coralSubsystem, ElevatorSubsystem elevatorSubsystem, ReefPosition reefPosition) {
+public class ScoreCoral extends LoggableSequentialCommandGroup {
+  public ScoreCoral(CoralSubsystem coralSubsystem, ElevatorSubsystem elevatorSubsystem, ReefPosition reefPosition) {
     super(
         new ShootCoral(coralSubsystem, Constants.CORAL_SHOOTER_SPEED),
         new SetElevatorStoredPosition(reefPosition, elevatorSubsystem));
