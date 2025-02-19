7cdc5bad33b8603c04156b2f94db07ffb5746685
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-02-19 17:58:52 -0500

    fixed pr comments

--- src/main/java/frc/robot/commands/sequences/IntakeAlgae.java
+++ src/main/java/frc/robot/commands/sequences/IntakeAlgae.java
@@ -1,6 +1,6 @@
 package frc.robot.commands.sequences;
 
-import frc.robot.commands.hihi.RetractHiHi;
+import frc.robot.commands.hihi.ExtendHiHi;
 import frc.robot.commands.hihi.RollHiHiRollerIn;
 import frc.robot.subsystems.hihiextender.HihiExtenderSubsystem;
 import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
@@ -9,7 +9,7 @@ import frc.robot.subsystems.hihiroller.HihiRollerSubsystem;
 public class IntakeAlgae extends LoggableParallelCommandGroup{
     public IntakeAlgae(HihiExtenderSubsystem hihiExtenderSubsystem, HihiRollerSubsystem hihiRollerSubsystem) {
         super(
-            new RetractHiHi(hihiExtenderSubsystem),
+            new ExtendHiHi(hihiExtenderSubsystem),
             new RollHiHiRollerIn(hihiRollerSubsystem)
         );
     }
