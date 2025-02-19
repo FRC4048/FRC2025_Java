d2978006c734cedd52dc1e114f673ec228bb9022
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-02-19 10:54:17 -0500

    Constants updated with correct CanIDS

---
+++ src/main/java/frc/robot/commands/hihi/SpinHihiExtenderTest.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.hihi;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.constants.Constants;
+import frc.robot.subsystems.hihiextender.HihiExtenderSubsystem;
+import frc.robot.utils.logging.commands.LoggableCommand;
+
+public class SpinHihiExtenderTest extends LoggableCommand {
+  /** Creates a new SpinHihiExtender. */
+  private final HihiExtenderSubsystem extender;
+
+  private final double speedMotors;
+  private final Timer timer;
+
+  public SpinHihiExtenderTest(HihiExtenderSubsystem extender, double speedMotors) {
+    this.speedMotors = speedMotors;
+    this.extender = extender;
+    timer = new Timer();
+    addRequirements(extender);
+  }
+
+  @Override
+  public void initialize() {
+    extender.setExtenderSpeed(speedMotors);
+    timer.restart();
+  }
+
+  @Override
+  public void execute() {}
+
+  @Override
+  public void end(boolean interrupted) {
+    extender.stopExtenderMotors();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(Constants.HIHI_EXTEND_TIMEOUT);
+  }
+}
