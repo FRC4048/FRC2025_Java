4c5268cd568d81c3ab07dd9d8d97891d1466c5bf
Joseph Kantrowitz <josephkantrowitz18@gmail.com>
2025-02-20 12:18:41 -0500

    added new functions to xboxcontroller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -177,6 +177,9 @@ public class RobotContainer {
     controller.a().onTrue(new ByeByeToFwrLimit(byebyeTilt));
     controller.b().onTrue(new ByeByeToRevLimit(byebyeTilt));
     controller.back().onTrue(new CancelAll(elevatorSubsystem, hihiExtender));
+    controller.leftStick().onTrue(new RollHiHiRollerIn(hihiRoller));
+    controller.rightStick().onTrue(new ShootHiHiRollerOut(hihiRoller));
+    controller.start().onTrue(new SpinRollerByeBye(byebyeRoller, Constants.BYEBYE_ROLLER_SPEED));
     // climber on Right Trigger
     if (Constants.COMMAND_DEBUG) {
       SmartShuffleboard.putCommand("DEBUG", "Roll Algae", new RollAlgae(hihiRoller, 0.5));
