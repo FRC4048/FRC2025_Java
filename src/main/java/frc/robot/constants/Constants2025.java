5b03eac64a3d3d47b14a06fe004537c7f3d02c9b
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-02-19 19:13:25 -0500

    Constants 2025 updated with new can ids (#190)
    * Constants 2025 updated with new can ids
    
    * merge conflicts
    
    * I cant type
    
    ---------
    
    Co-authored-by: codetoad <softwareenginer@pm.me>

--- src/main/java/frc/robot/constants/Constants2025.java
+++ src/main/java/frc/robot/constants/Constants2025.java
@@ -4,24 +4,21 @@ public class Constants2025 extends GameConstants {
   /** Constants2025 is only for CANIDs and nothing else, everything else goes into GameConstants. */
 
   // Elevator
-  public static final int ELEVATOR_MOTOR_ID = 0; // TODO: change later
-
-  public static final int ELEVATOR_MOTOR_2_ID = 1; // TODO: change later
+  public static final int ELEVATOR_MOTOR_ID = 35;
 
   // Coral Shooter
-  public static final int SHOOTER_MOTOR_LEADER_ID = 44; // TODO: change later
-  public static final int SHOOTER_MOTOR_FOLLOWER_ID = 45; // TODO: change later
-  public static final int SHOOTER_TILT_MOTOR_ID = 4; // TODO: change later
+  public static final int SHOOTER_MOTOR_LEADER_ID = 8;
+  public static final int SHOOTER_MOTOR_FOLLOWER_ID = 53;
 
   // Algae Roller
-  public static final int ALGAE_ROLLER_CAN_ID = 5; // TODO: change later
+  public static final int ALGAE_ROLLER_CAN_ID = 2;
 
   // Algae Extender
-  public static final int ALGAE_EXTENDER_MOTOR_ID = 6; // TODO: change later
+  public static final int ALGAE_EXTENDER_MOTOR_ID = 3;
 
   // Algae Bye-Bye
-  public static final int ALGAE_BYEBYE_SPINING_ID = 10; // TODO: change later
-  public static final int ALGAE_BYEBYE_TILT_ID = 2; // TODO: change later
+  public static final int ALGAE_BYEBYE_SPINING_ID = 5;
+  public static final int ALGAE_BYEBYE_TILT_ID = 1;
 
   // Climber
   public static final int CLIMBER_MOTOR_ID = 9; // TODO: change later
@@ -30,18 +27,18 @@ public class Constants2025 extends GameConstants {
   public static final int LIGHTSTRIP_PORT = 0; // TODO: change later
 
   // Drive
-  public static final int DRIVE_FRONT_RIGHT_D = 34; // TODO: change later
-  public static final int DRIVE_BACK_RIGHT_D = 31; // TODO: change later
-  public static final int DRIVE_FRONT_LEFT_D = 56; // TODO: change later
-  public static final int DRIVE_BACK_LEFT_D = 51; // TODO: change later
-  public static final int DRIVE_CANCODER_FRONT_RIGHT = 12; // TODO: change later
-  public static final int DRIVE_CANCODER_BACK_RIGHT = 13; // TODO: change later
-  public static final int DRIVE_CANCODER_FRONT_LEFT = 11; // TODO: change later
-  public static final int DRIVE_CANCODER_BACK_LEFT = 14; // TODO: change later
+  public static final int DRIVE_FRONT_RIGHT_D = 34;
+  public static final int DRIVE_BACK_RIGHT_D = 31;
+  public static final int DRIVE_FRONT_LEFT_D = 56;
+  public static final int DRIVE_BACK_LEFT_D = 51;
+  public static final int DRIVE_CANCODER_FRONT_RIGHT = 12;
+  public static final int DRIVE_CANCODER_BACK_RIGHT = 13;
+  public static final int DRIVE_CANCODER_FRONT_LEFT = 11;
+  public static final int DRIVE_CANCODER_BACK_LEFT = 14;
 
   // Steer
-  public static final int DRIVE_FRONT_RIGHT_S = 46; // TODO: change later
-  public static final int DRIVE_BACK_RIGHT_S = 43; // TODO: change later
-  public static final int DRIVE_FRONT_LEFT_S = 55; // TODO: change later
-  public static final int DRIVE_BACK_LEFT_S = 49; // TODO: change later
+  public static final int DRIVE_FRONT_RIGHT_S = 46;
+  public static final int DRIVE_BACK_RIGHT_S = 43;
+  public static final int DRIVE_FRONT_LEFT_S = 55;
+  public static final int DRIVE_BACK_LEFT_S = 49;
 }
