3312ccb093752a969ee3bc27b4045dbb1689af4f
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-02-20 10:27:12 -0500

    kjn

--- src/main/java/frc/robot/utils/MotorName.java
+++ src/main/java/frc/robot/utils/MotorName.java
@@ -1,15 +1,15 @@
 package frc.robot.utils;
 
 public enum MotorName {
-    Neo(40),
-    Neo550(20);
-    public final double currentLimit;
+  Neo(40),
+  Neo550(20);
+  public final double currentLimit;
 
-    MotorName(double currentLimit) {
-        this.currentLimit = currentLimit;
-    }
+  MotorName(double currentLimit) {
+    this.currentLimit = currentLimit;
+  }
 
-    public double getCurrentLimit() {
-        return currentLimit;
-    }
+  public double getCurrentLimit() {
+    return currentLimit;
+  }
 }
