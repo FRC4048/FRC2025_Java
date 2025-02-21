18c658498a9d8b1d4a3583b05f6bf78d2cfefd60
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-02-20 19:52:18 -0500

    fixed some pr

--- src/main/java/frc/robot/utils/diag/DiagSwitch.java
+++ src/main/java/frc/robot/utils/diag/DiagSwitch.java
@@ -2,26 +2,24 @@ package frc.robot.utils.diag;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
-/**
- * Diagnostics class for digital switch; it is a DiagBoolean object.
- */
+/** Diagnostics class for digital switch; it is a DiagBoolean object. */
 public class DiagSwitch extends DiagBoolean {
 
-    private DigitalInput digitalInput;
+  private DigitalInput digitalInput;
 
-    /**
-     * Constructor
-     *
-     * @param name            the name of the unit. Will be used on the Shuffleboard
-     * @param digitalInput    - the DigitalInput the switch is connected to
-     */
-    public DiagSwitch(String title, String name, DigitalInput digitalInput) {
-        super(title, name);
-        this.digitalInput = digitalInput;
-    }
+  /**
+   * Constructor
+   *
+   * @param name the name of the unit. Will be used on the Shuffleboard
+   * @param digitalInput - the DigitalInput the switch is connected to
+   */
+  public DiagSwitch(String title, String name, DigitalInput digitalInput) {
+    super(title, name);
+    this.digitalInput = digitalInput;
+  }
 
-    @Override
-    protected boolean getValue() {
-        return digitalInput.get();
-    }
+  @Override
+  protected boolean getValue() {
+    return digitalInput.get();
+  }
 }
