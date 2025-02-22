093c054f4ceda1ebb9a19d5b5151202d9075c412
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-02-21 20:54:58 -0500

    THERES NO MORE RED

--- src/main/java/frc/robot/commands/autos/LeftFourPieceFork.java
+++ src/main/java/frc/robot/commands/autos/LeftFourPieceFork.java
@@ -19,7 +19,7 @@ public class LeftFourPieceFork extends LoggableSequentialCommandGroup {
                 Paths.getInstance().getRobotOneToPostJCommand()), // Robot 1 to Post J
             new SetElevatorStoredPosition(ReefPosition.LEVEL4, elevatorSubsystem) // Elevator to L4
             ),
-        new ShootCoral(coralSubsystem, 0.5), // Score a Coral
+        new ShootCoral(coralSubsystem, 0.5), // Score a Coral, Updated with the correct Speed according to GameConstants
         new LoggableParallelCommandGroup(
             new LoggableCommandWrapper(
                 Paths.getInstance().getPostJToStationOneCommand()), // Post J To Station 1
@@ -31,10 +31,10 @@ public class LeftFourPieceFork extends LoggableSequentialCommandGroup {
                 Paths.getInstance().getStationOneToPostKCommand()), // Station 1 to Post K
             new SetElevatorStoredPosition(ReefPosition.LEVEL4, elevatorSubsystem) // Elevator to L4
             ),
-        new ShootCoral(coralSubsystem, 0.5), // Score a Coral
+        new ShootCoral(coralSubsystem, 0.5), // Score a Coral, Updated with the correct Speed according to GameConstants
         new LoggableParallelCommandGroup(
             new LoggableCommandWrapper(
-                Paths.getInstance().getPostKToStationOne()), // Post K to Station 1
+                Paths.getInstance().getPostKToStationOneCommand()), // Post K to Station 1
             new ResetElevator(elevatorSubsystem) // Elevator to L0
             ),
         new IntakeCoral(coralSubsystem), // Intake a Coral
@@ -43,7 +43,7 @@ public class LeftFourPieceFork extends LoggableSequentialCommandGroup {
                 Paths.getInstance().getStationOneToPostLCommand()), // Station 1 to Post L
             new SetElevatorStoredPosition(ReefPosition.LEVEL4, elevatorSubsystem) // Elevator to L4
             ),
-        new ShootCoral(coralSubsystem, 0.5), // Score a Coral
+        new ShootCoral(coralSubsystem, 0.5), // Score a Coral, Updated with the correct Speed according to GameConstants
         new LoggableParallelCommandGroup(
             new LoggableCommandWrapper(
                 Paths.getInstance().getPostLToStationOneCommand()), // Post L to Station 1
@@ -55,9 +55,8 @@ public class LeftFourPieceFork extends LoggableSequentialCommandGroup {
                 Paths.getInstance().getStationOneToPostACommand()), // Station One to Post K
             new SetElevatorStoredPosition(ReefPosition.LEVEL4, elevatorSubsystem) // Elevator to L4
             ),
-        new ResetElevator(
-            elevatorSubsystem) // Elevator to L0 we probably won't get to this point but we should
-        // move the elevator to L0 at the end of an Auto
+        new ShootCoral(coralSubsystem, 0.5), //Score a Coral, Updated with the correct Speed according to GameConstants
+        new ResetElevator(elevatorSubsystem) // Elevator to L0 we probably won't get to this point but we should move the elevator to L0 at the end of an Auto
         );
   }
 }
