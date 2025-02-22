b85ac3e4593f14271b806e5510b95fa77c6c474e
jamthepersonj <wessjackson93@gmail.com>
2025-02-22 11:27:05 -0500

    update

--- src/main/java/frc/robot/constants/ConstantsChassis2024.java
+++ src/main/java/frc/robot/constants/ConstantsChassis2024.java
@@ -5,20 +5,25 @@
 package frc.robot.constants;
 
 /** Add your docs here. */
-public class ConstantsChassis2024 extends Constants2025{
-// Drive
-public static final int DRIVE_FRONT_RIGHT_D = 59;
-public static final int DRIVE_BACK_RIGHT_D = 60;
-public static final int DRIVE_FRONT_LEFT_D = 58;
-public static final int DRIVE_BACK_LEFT_D = 57;
-public static final int DRIVE_CANCODER_FRONT_RIGHT = 39;
-public static final int DRIVE_CANCODER_BACK_RIGHT = 40;
-public static final int DRIVE_CANCODER_FRONT_LEFT = 38;
-public static final int DRIVE_CANCODER_BACK_LEFT = 37;
+public class ConstantsChassis2024 extends Constants2025 {
+  // Drive
+  public static final int DRIVE_FRONT_RIGHT_D = 59;
+  public static final int DRIVE_BACK_RIGHT_D = 60;
+  public static final int DRIVE_FRONT_LEFT_D = 58;
+  public static final int DRIVE_BACK_LEFT_D = 57;
+  public static final int DRIVE_CANCODER_FRONT_RIGHT = 39;
+  public static final int DRIVE_CANCODER_BACK_RIGHT = 40;
+  public static final int DRIVE_CANCODER_FRONT_LEFT = 38;
+  public static final int DRIVE_CANCODER_BACK_LEFT = 37;
 
-// Steer
-public static final int DRIVE_FRONT_RIGHT_S = 29;
-public static final int DRIVE_BACK_RIGHT_S = 30;
-public static final int DRIVE_FRONT_LEFT_S = 28;
-public static final int DRIVE_BACK_LEFT_S = 27;
+  // Steer
+  public static final int DRIVE_FRONT_RIGHT_S = 29;
+  public static final int DRIVE_BACK_RIGHT_S = 30;
+  public static final int DRIVE_FRONT_LEFT_S = 28;
+  public static final int DRIVE_BACK_LEFT_S = 27;
+
+  public static final double BACK_RIGHT_ABS_ENCODER_ZERO = 261.56;
+  public static final double FRONT_LEFT_ABS_ENCODER_ZERO = 190.28;
+  public static final double BACK_LEFT_ABS_ENCODER_ZERO = 78.22;
+  public static final double FRONT_RIGHT_ABS_ENCODER_ZERO = 306.29;
 }
