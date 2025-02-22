b36b300820225f5667479abba808365e69d6232f
Joseph Kantrowitz <josephkantrowitz18@gmail.com>
2025-02-22 14:52:51 -0500

    added elevator diags

--- src/main/java/frc/robot/subsystems/elevator/RealElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/RealElevatorIO.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems.elevator;
 
 import com.revrobotics.spark.SparkBase;
+import frc.robot.Robot;
 import frc.robot.constants.Constants;
+import frc.robot.utils.diag.DiagSparkMaxEncoder;
+import frc.robot.utils.diag.DiagSparkMaxSwitch;
 import frc.robot.utils.logging.subsystem.inputs.PidMotorInputs;
 import frc.robot.utils.logging.subsystem.providers.NeoPidMotorInputProvider;
 import frc.robot.utils.motor.NeoPidConfig;
@@ -15,6 +18,28 @@ public class RealElevatorIO implements ElevatorIO {
     this.elevatorMotor =
         new NeoPidMotor(Constants.ELEVATOR_MOTOR_ID, Constants.ELEVATOR_USE_MAX_MOTION);
     this.inputProvider = new NeoPidMotorInputProvider(elevatorMotor);
+    Robot.getDiagnostics()
+        .addDiagnosable(
+            new DiagSparkMaxSwitch(
+                "Elevator",
+                "ElevatorForwardLimit",
+                elevatorMotor.getNeoMotor(),
+                DiagSparkMaxSwitch.Direction.FORWARD));
+    Robot.getDiagnostics()
+        .addDiagnosable(
+            new DiagSparkMaxSwitch(
+                "Elevator",
+                "ElevatorReverseLimit",
+                elevatorMotor.getNeoMotor(),
+                DiagSparkMaxSwitch.Direction.REVERSE));
+    Robot.getDiagnostics()
+        .addDiagnosable(
+            new DiagSparkMaxEncoder(
+                "Elevator", "ElevatorForwardEncoder", 10, elevatorMotor.getNeoMotor()));
+    Robot.getDiagnostics()
+        .addDiagnosable(
+            new DiagSparkMaxEncoder(
+                "Elevator", "ElevatorReverseEncoder", 10, elevatorMotor.getNeoMotor()));
   }
 
   @Override
