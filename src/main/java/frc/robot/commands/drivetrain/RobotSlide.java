39ed38ddbb8c92f49f8372f64815dc71213f0547
bengold22 <98672229+bengold22@users.noreply.github.com>
2025-02-23 18:29:11 -0500

    Fixing direction

--- src/main/java/frc/robot/commands/drivetrain/RobotSlide.java
+++ src/main/java/frc/robot/commands/drivetrain/RobotSlide.java
@@ -21,7 +21,7 @@ public class RobotSlide extends LoggableCommand {
 
   @Override
   public void execute() {
-    double horizVal = horizSupplier.getAsDouble();
+    double horizVal = -horizSupplier.getAsDouble();
     double str = MathUtil.applyDeadband(horizVal, 0.05) * Constants.MAX_VELOCITY;
     ChassisSpeeds speeds = drivetrain.createChassisSpeeds(0, str, 0, DriveMode.ROBOT_CENTRIC);
     drivetrain.drive(speeds);
