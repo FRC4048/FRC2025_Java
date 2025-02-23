097d236a48e0caa81a299680b8851f2c0bc33506
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-02-23 15:19:19 -0500

    updated all the speeds to 3.53

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2025_Java2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 210;
-  public static final String GIT_SHA = "4a3c825463eb839e1406a54666379dca08137685";
-  public static final String GIT_DATE = "2025-02-23 15:02:23 EST";
+  public static final int GIT_REVISION = 211;
+  public static final String GIT_SHA = "985220332f3c94cf1b6d4d8f392670e20684ec99";
+  public static final String GIT_DATE = "2025-02-23 15:02:44 EST";
   public static final String GIT_BRANCH = "MK-path";
-  public static final String BUILD_DATE = "2025-02-23 15:02:29 EST";
-  public static final long BUILD_UNIX_TIME = 1740340949693L;
+  public static final String BUILD_DATE = "2025-02-23 15:02:55 EST";
+  public static final long BUILD_UNIX_TIME = 1740340975394L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
