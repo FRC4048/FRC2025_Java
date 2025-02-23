4a3c825463eb839e1406a54666379dca08137685
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-02-23 15:02:23 -0500

    adding left diagonal test

---
+++ src/main/java/frc/robot/commands/autos/LeftDiagonalTest.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.autos;
+
+import frc.robot.commands.coral.ShootCoral;
+import frc.robot.commands.elevator.SetElevatorStoredPosition;
+import frc.robot.constants.ElevatorPositions;
+import frc.robot.subsystems.coral.CoralSubsystem;
+import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.utils.logging.commands.LoggableCommandWrapper;
+import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
+
+public class LeftDiagonalTest extends LoggableSequentialCommandGroup {
+  public LeftDiagonalTest(
+      ElevatorSubsystem elevatorSubsystem,
+      CoralSubsystem coralSubsystem) {
+    super(
+        new LoggableCommandWrapper(Paths.getInstance().getRobotTwoToPostHCommand()),
+        new LoggableCommandWrapper(new SetElevatorStoredPosition(ElevatorPositions.LEVEL1, elevatorSubsystem)),
+        new LoggableCommandWrapper(new ShootCoral(coralSubsystem, 0.5)) //Updated with GameConstants
+  );
+  }
+}
+
