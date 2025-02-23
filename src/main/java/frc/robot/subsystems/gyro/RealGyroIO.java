94d351cbf3b6cca98684e805619ea6dfa640235c
Aditya Hebbar <Adityarhebbar@gmail.com>
2025-02-23 12:57:07 -0500

    Added a Gyro diag

--- src/main/java/frc/robot/subsystems/gyro/RealGyroIO.java
+++ src/main/java/frc/robot/subsystems/gyro/RealGyroIO.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems.gyro;
 
+import frc.robot.Robot;
+import frc.robot.utils.diag.DiagGyro;
+
 public class RealGyroIO implements GyroIO {
   private final ThreadedGyro gyro;
 
   public RealGyroIO(ThreadedGyro gyro) {
     this.gyro = gyro;
+    Robot.getDiagnostics().addDiagnosable(new DiagGyro("Diag", "Gyro", 0, gyro));
   }
 
   @Override
