6e8714bf613755617997fb569146a0ea8b26b847
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-02-23 18:15:18 -0500

    more pathplanner

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2025_Java2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 216;
-  public static final String GIT_SHA = "fa3bdf967b5c6bde36fffc15571ec902bfadbafb";
-  public static final String GIT_DATE = "2025-02-23 15:21:05 EST";
+  public static final int GIT_REVISION = 217;
+  public static final String GIT_SHA = "e6cdf941965af0e7e2876bdfaa2c8acf6cd814dc";
+  public static final String GIT_DATE = "2025-02-23 17:21:26 EST";
   public static final String GIT_BRANCH = "MK-path";
-  public static final String BUILD_DATE = "2025-02-23 17:21:00 EST";
-  public static final long BUILD_UNIX_TIME = 1740349260493L;
+  public static final String BUILD_DATE = "2025-02-23 17:28:33 EST";
+  public static final long BUILD_UNIX_TIME = 1740349713208L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
