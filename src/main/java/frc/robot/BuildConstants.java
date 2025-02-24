692ad41030891e1a624f34474390231ef8884b71
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-02-24 17:04:19 -0500

    done

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC2025_Java";
+  public static final String MAVEN_NAME = "FRC2025_Java2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 223;
-  public static final String GIT_SHA = "ef27247eea2f715ccbcc2e785ed1fd180608d9de";
-  public static final String GIT_DATE = "2025-02-23 19:48:33 EST";
+  public static final int GIT_REVISION = 224;
+  public static final String GIT_SHA = "08219c5359b643d8220c79694cb3e7ab9ba5d975";
+  public static final String GIT_DATE = "2025-02-23 19:51:10 EST";
   public static final String GIT_BRANCH = "MK-path";
-  public static final String BUILD_DATE = "2025-02-23 19:50:37 EST";
-  public static final long BUILD_UNIX_TIME = 1740358237698L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-24 09:22:39 EST";
+  public static final long BUILD_UNIX_TIME = 1740406959381L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
