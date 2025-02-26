f2bac5160ab6a72961b92cbcef54364041ef9bcf
jamthepersonj <wessjackson93@gmail.com>
2025-02-25 20:14:29 -0500

    cleaned up the one command

--- src/main/java/frc/robot/commands/hihi/ExtendHiHi.java
+++ src/main/java/frc/robot/commands/hihi/ExtendHiHi.java
@@ -1,27 +1,20 @@
 package frc.robot.commands.hihi;
 
-import edu.wpi.first.wpilibj.Timer;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.hihiextender.HihiExtenderSubsystem;
-import frc.robot.utils.logging.TimeoutLogger;
 import frc.robot.utils.logging.commands.LoggableCommand;
 
 public class ExtendHiHi extends LoggableCommand {
   private final HihiExtenderSubsystem hihiExtender;
-  private Timer timer;
-  private final TimeoutLogger timeoutCounter;
 
   public ExtendHiHi(HihiExtenderSubsystem hihiExtender) {
     this.hihiExtender = hihiExtender;
-    timeoutCounter = new TimeoutLogger("ExtendHiHi");
-    timer = new Timer();
     addRequirements(hihiExtender);
   }
 
   @Override
   public void initialize() {
     hihiExtender.setExtenderPosition(Constants.HIHI_EXTEND_POSITION);
-    timer.restart();
   }
 
   @Override
