bea61acdd93145abce3ed708ecb218e926d504f7
codetoad <softwareenginer@pm.me>
2025-02-26 18:26:17 -0500

    fix

--- src/main/java/frc/robot/constants/GameConstants.java
+++ src/main/java/frc/robot/constants/GameConstants.java
@@ -98,9 +98,9 @@ public class GameConstants {
   }
 
   // Limits
-  public static final int DRIVE_SMART_LIMIT = 38; // TODO: change later
-  public static final int DRIVE_SECONDARY_LIMIT = 48; // TODO: change later
-  public static final double DRIVE_RAMP_RATE_LIMIT = 0.1; // TODO: change later
+  public static final int DRIVE_SMART_LIMIT = 40; // TODO: change later
+  public static final int DRIVE_SECONDARY_LIMIT = 0; // TODO: change later
+  public static final double DRIVE_RAMP_RATE_LIMIT = 0; // TODO: change later
   public static final int NEO_CURRENT_LIMIT = 20;
   public static final int HIHI_CURRENT_LIMIT = 10;
 
@@ -126,11 +126,13 @@ public class GameConstants {
   public static final boolean ELEVATOR_USE_MAX_MOTION = true;
 
   // Drive PID
-  public static final double DRIVE_PID_P = 1; // TODO: change later
+  public static final double DRIVE_PID_P = 2; // TODO: change later
   public static final double DRIVE_PID_I = 0; // TODO: change later
   public static final double DRIVE_PID_D = 0; // TODO: change later
-  public static final double DRIVE_PID_FF_S = 1; // TODO: change later
-  public static final double DRIVE_PID_FF_V = 2.8; // TODO: change later
+  public static final double DRIVE_PID_FF_S = 0.19;
+  public static final double DRIVE_PID_FF_V = 3.3;
+  public static final double DRIVE_PID_I_ZONE = 0; // TODO: change later
+  public static final double DRIVE_PID_ALLOWED_ERROR = 0;
 
   // Steer PID
   public static final double STEER_PID_P = 0.3; // TODO: change later
