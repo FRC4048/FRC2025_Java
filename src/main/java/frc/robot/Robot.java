3d0065a3b57d9c925f4516a8830599d2130a1c1e
jamthepersonj <wessjackson93@gmail.com>
2025-03-02 16:33:46 -0500

    merged

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@ import com.pathplanner.lib.pathfinding.Pathfinding;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.camera.CameraThread;
 import frc.robot.commands.drivetrain.ResetGyro;
 import frc.robot.commands.drivetrain.SetBaseVisionStd;
@@ -105,9 +104,9 @@ public class Robot extends LoggedRobot {
 
   /** Use this instead of robot init. */
   private void actualInit() {
-    new SequentialCommandGroup(
-            new WheelAlign(m_robotContainer.getDrivetrain()),
-            new ResetGyro(m_robotContainer.getDrivetrain()))
+    new LoggableSequentialCommandGroup(
+            new WheelAlign(robotContainer.getDrivetrain()),
+            new ResetGyro(robotContainer.getDrivetrain()))
         .schedule();
   }
 
