d8a8c35510e3b7e0ca839769baa692e7b028fd50
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-03-02 15:55:47 -0500

    fixed small errors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -176,6 +176,6 @@ public class Robot extends LoggedRobot {
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {
-    m_robotContainer.updateSimulation();
+    robotContainer.updateSimulation();
   }
 }
