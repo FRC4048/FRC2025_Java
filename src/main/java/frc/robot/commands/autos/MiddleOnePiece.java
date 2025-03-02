5a4810298ac51bb6f8c22f7e0b5e768536a1af77
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-03-02 14:22:35 -0500

    fixed middle path

--- src/main/java/frc/robot/commands/autos/MiddleOnePiece.java
+++ src/main/java/frc/robot/commands/autos/MiddleOnePiece.java
@@ -1,21 +1,28 @@
 package frc.robot.commands.autos;
 
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.byebye.ByeByeToFwrLimit;
-import frc.robot.subsystems.algaebyebyetilt.AlgaeByeByeTiltSubsystem;
+import frc.robot.commands.coral.ShootCoral;
+import frc.robot.commands.elevator.SetElevatorStoredPosition;
+import frc.robot.constants.ElevatorPosition;
 import frc.robot.subsystems.coral.CoralSubsystem;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
+import frc.robot.subsystems.lightStrip.LightStrip;
 import frc.robot.utils.logging.commands.LoggableCommandWrapper;
+import frc.robot.utils.logging.commands.LoggableParallelCommandGroup;
 import frc.robot.utils.logging.commands.LoggableSequentialCommandGroup;
+import frc.robot.utils.logging.commands.LoggableWaitCommand;
 
 public class MiddleOnePiece extends LoggableSequentialCommandGroup {
   public MiddleOnePiece(
-      ElevatorSubsystem elevatorSubsystem,
-      CoralSubsystem coralSubsystem,
-      AlgaeByeByeTiltSubsystem algaeByeByeTiltSubsystem) {
+  ElevatorSubsystem elevator, CoralSubsystem coral, LightStrip lightStrip) {
+  
     super(
+        new LoggableParallelCommandGroup(
         new LoggableCommandWrapper(Paths.getInstance().getRobotTwoToPostHCommand()),
-        new LoggableCommandWrapper(new WaitCommand(11.79)),
-        new ByeByeToFwrLimit(algaeByeByeTiltSubsystem));
+        new SetElevatorStoredPosition(ElevatorPosition.LEVEL4, elevator, lightStrip),
+        new LoggableSequentialCommandGroup(
+            new LoggableWaitCommand(1.25),
+            new GoAndWaitAtElevatorPosition(elevator, ElevatorPosition.LEVEL4))),
+      new ShootCoral(coral, 0.5)
+    );
   }
 }
