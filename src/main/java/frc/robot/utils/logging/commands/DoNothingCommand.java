a48f3c673ef08cfff5cccb1920c014f9fb3d9463
Michael Kovalev <134699174+michaelk036@users.noreply.github.com>
2025-03-03 20:18:49 -0500

    doesn't work yet

--- src/main/java/frc/robot/utils/logging/commands/DoNothingCommand.java
+++ src/main/java/frc/robot/utils/logging/commands/DoNothingCommand.java
@@ -1,8 +1,16 @@
 package frc.robot.utils.logging.commands;
 
+import edu.wpi.first.math.geometry.Pose2d;
+
 public class DoNothingCommand extends LoggableCommand {
+  
   @Override
   public boolean isFinished() {
     return true;
+
+  @Override
+  public Pose2d getStartingPosition() {
+    return provider.getSelectedLocation().getLocation();
+  }
   }
 }
