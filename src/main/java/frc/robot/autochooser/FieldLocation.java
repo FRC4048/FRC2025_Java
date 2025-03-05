0e682663710ace2bad4574f7b06ea5f54be6e436
codetoad <softwareenginer@pm.me>
2025-03-04 22:30:09 -0500

    fixed Duplicate key execption. The fix was to remove the who red and blue name thing because the names are always the same.

--- src/main/java/frc/robot/autochooser/FieldLocation.java
+++ src/main/java/frc/robot/autochooser/FieldLocation.java
@@ -2,7 +2,6 @@ package frc.robot.autochooser;
 
 import static edu.wpi.first.wpilibj.DriverStation.Alliance;
 
-import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
@@ -13,30 +12,28 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum FieldLocation {
-  ZERO(0, 0, 0, "Zero", "Zero"),
-  INVALID(-1, -1, -1, "INVALID", "INVALID"),
-  LEFT(7.150, 6.000, 180, "NON Processor Side", "NON Processor Side"),
-  MIDDLE(7.150, 4.516, 180, "Middle", "Middle"),
-  RIGHT(7.150, 2.000, 180, "Processor Side", "Processor Side");
+  ZERO(0, 0, 0, "Zero"),
+  INVALID(-1, -1, -1, "INVALID"),
+  LEFT(7.150, 6.000, 180, "NON Processor Side"),
+  MIDDLE(7.150, 4.516, 180, "Middle"),
+  RIGHT(7.150, 2.000, 180, "Processor Side");
 
   private static final double RED_X_POS = 2.3876; // meters
   private static final double HEIGHT_OF_FIELD = 8.05;
   private final double yPose;
   private final double xPose;
   private final double angle;
-  private final String blueName;
-  private final String redName;
+  private final String name;
   private static final HashMap<String, FieldLocation> nameMap =
       new HashMap<>(
           Arrays.stream(FieldLocation.values())
               .collect(Collectors.toMap(FieldLocation::getShuffleboardName, Function.identity())));
 
-  FieldLocation(double xPos, double yPose, double angle, String blueName, String redName) {
+  FieldLocation(double xPos, double yPose, double angle, String name) {
     this.xPose = xPos;
     this.yPose = yPose;
     this.angle = angle;
-    this.blueName = blueName;
-    this.redName = redName;
+    this.name = name;
   }
 
   public static FieldLocation fromName(String string) {
@@ -46,9 +43,8 @@ public enum FieldLocation {
   public Pose2d getLocation() {
     Alliance alliance = Robot.getAllianceColor().orElse(null);
     if (alliance == null) {
-      return new Pose2d(0.0, 0.0, Rotation2d.kZero);
+      return new Pose2d(INVALID.xPose, INVALID.yPose, Rotation2d.fromDegrees(INVALID.angle));
     }
-
     double x = (alliance == Alliance.Red) ? xPose + RED_X_POS + Units.inchesToMeters(36) : xPose;
     double y = (alliance == Alliance.Red) ? yPose - 2 * (yPose - (HEIGHT_OF_FIELD / 2)) : yPose;
     double radian =
@@ -57,10 +53,6 @@ public enum FieldLocation {
   }
 
   public String getShuffleboardName() {
-    Alliance alliance = Robot.getAllianceColor().orElseGet(() -> null);
-    if (alliance == null) {
-      return "INVALID";
-    }
-    return (alliance == Alliance.Red) ? redName : blueName;
+    return name;
   }
 }
