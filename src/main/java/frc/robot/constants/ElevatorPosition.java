e808bfc9229eca25d5c67c8bb91a3ffaba6507d2
jamthepersonj <wessjackson93@gmail.com>
2025-03-12 19:48:38 -0400

    Updated speeds

--- src/main/java/frc/robot/constants/ElevatorPosition.java
+++ src/main/java/frc/robot/constants/ElevatorPosition.java
@@ -2,10 +2,10 @@ package frc.robot.constants;
 
 public enum ElevatorPosition {
   CORAL_INTAKE(-1, 0.0), // shoot speed for intake is not used
-  LEVEL1(-3.0, .8),
+  LEVEL1(-3.0, .4),
   LEVEL2(-9.0, .8),
   LEVEL3(-24, .8),
-  LEVEL4(-47, .6);
+  LEVEL4(-47, .5);
 
   private final double heightElevator;
   private final double shootSpeed;
