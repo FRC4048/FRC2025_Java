8c03f7e50c3e4944061fa6904586287e1aeb35da
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-03-20 22:42:11 -0400

    just small constants changes

--- src/main/java/frc/robot/constants/GameConstants.java
+++ src/main/java/frc/robot/constants/GameConstants.java
@@ -203,8 +203,9 @@ public class GameConstants {
   public static final double ROBOT_MASS = 58.967; // In Kg, change later
   public static final double ROBOT_BUMPER_WIDTH = 0.914;
   public static final double ROBOT_BUMPER_LENGTH = 0.914;
-  public static final double STEER_ROTATIONAL_INERTIA = 0.0325; // TODO: change later
+  public static final double STEER_ROTATIONAL_INERTIA = 0.03; // TODO: change later
   public static final double COEFFICIENT_OF_FRICTION = 1.542;
+  public static final double STEER_FRICTION_VOLTAGE = 0.2;
 
   // ELEVATOR CONSTANTS
   public static final double ELEVATOR_MANUAL_DEADBAND = 0.2;
