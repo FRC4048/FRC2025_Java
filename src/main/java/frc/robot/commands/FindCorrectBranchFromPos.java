416b0409782038073df54a5f529c901d7edb4037
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-03-22 12:29:22 -0400

    MORE linear algebra

--- src/main/java/frc/robot/commands/FindCorrectBranchFromPos.java
+++ src/main/java/frc/robot/commands/FindCorrectBranchFromPos.java
@@ -3,82 +3,78 @@ package frc.robot.commands;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.numbers.N3;
 import frc.robot.constants.AlgaePositions;
 import frc.robot.constants.BranchPositions;
 import frc.robot.constants.Constants;
+import java.util.Arrays;
 
 public class FindCorrectBranchFromPos {
-  public static final double POS_TO_FOV_CONVERSION_X =
-      Math.tan(Constants.LIMELIGHT_HALF_FOV.getX()) / Constants.LIMELIGHT_HALF_POS.getX();
-  public static final double POS_TO_FOV_CONVERSION_Y =
-      Math.tan(Constants.LIMELIGHT_HALF_FOV.getY()) / Constants.LIMELIGHT_HALF_POS.getY();
   public static final double SPECIAL_X =
-      Constants.LIMELIGHT_HALF_POS.getX() / Math.tan(Constants.LIMELIGHT_HALF_FOV.getX());
+      Constants.LIMELIGHT_HALF_POS.get(0) / Math.tan(Constants.LIMELIGHT_HALF_FOV.get(0));
   public static final double SPECIAL_Y =
-      Constants.LIMELIGHT_HALF_POS.getY() / Math.tan(Constants.LIMELIGHT_HALF_FOV.getY());
+      Constants.LIMELIGHT_HALF_POS.get(0) / Math.tan(Constants.LIMELIGHT_HALF_FOV.get(0));
   public static final double SPECIAL_RATIO = SPECIAL_Y / SPECIAL_X;
   private static final BranchPositions[] BRANCHES = BranchPositions.values();
   private static final AlgaePositions[] ALGAES = AlgaePositions.values();
+  private static final Vector<N3>[] PRECOMPUTED_BRANCH_VECS =
+      Arrays.stream(BranchPositions.values())
+          .map(branch -> branch.getPosition().getTranslation().toVector())
+          .toArray(Vector[]::new);
+  private static final Vector<N3>[] PRECOMPUTED_ALGAE_VECS =
+      Arrays.stream(AlgaePositions.values())
+          .map(algae -> algae.getPosition().getTranslation().toVector())
+          .toArray(Vector[]::new);
 
-  public static BranchPositions UnitTest1() {
-    double x = -4.1902085050000 / 3.8337244850000 * SPECIAL_X + Constants.LIMELIGHT_HALF_POS.getX();
-    double y = -0.706978325 / 3.8337244850000 * SPECIAL_Y + Constants.LIMELIGHT_HALF_POS.getY();
-    return FindCoralBranch(new Pose2d(0, 0, new Rotation2d(0)), new Translation2d(x, y));
-  }
-
-  public static BranchPositions FindCoralBranch(Pose2d robotPos, Translation2d piecePos) {
+  public static BranchPositions FindCoralBranch(Pose2d robotPos, Vector<N2> piecePos) {
     final Pose3d cameraPos = new Pose3d(robotPos).transformBy(Constants.CAMERA_TO_ROBOT);
-    final Rotation3d invCameraRotation = cameraPos.getRotation().unaryMinus();
-    final Translation2d pieceTranslation = Constants.LIMELIGHT_HALF_POS.minus(piecePos);
+    final Vector<N3> cameraPosVec = cameraPos.getTranslation().toVector();
+    final Vector<N3> invCameraRotation = cameraPos.getRotation().unaryMinus().toVector();
+    final Vector<N2> pieceTranslation = Constants.LIMELIGHT_HALF_POS.minus(piecePos);
     final Vector<N3> pieceVec =
-        VecBuilder.fill(SPECIAL_Y, pieceTranslation.getX() * SPECIAL_RATIO, pieceTranslation.getY())
+        VecBuilder.fill(SPECIAL_Y, pieceTranslation.get(0) * SPECIAL_RATIO, pieceTranslation.get(1))
             .unit();
     double maxDot = -1.0;
     BranchPositions closestBranch = null;
-    for (BranchPositions branch : BRANCHES) {
+    for (int i = 0; i < BRANCHES.length; i++) {
       Vector<N3> branchVec =
-          branch
-              .getPosition()
-              .minus(cameraPos)
-              .getTranslation()
-              .rotateBy(invCameraRotation)
-              .toVector()
-              .unit();
+          Vector.cross(PRECOMPUTED_BRANCH_VECS[i].minus(cameraPosVec), invCameraRotation).unit();
       double dot = pieceVec.dot(branchVec);
       if (dot > maxDot) {
         maxDot = dot;
-        closestBranch = branch;
+        closestBranch = BRANCHES[i];
       }
     }
     return closestBranch;
   }
 
-  public static AlgaePositions FindAlgae(Pose2d robotPos, Translation2d piecePos) {
+  public static AlgaePositions FindAlgae(Pose2d robotPos, Vector<N2> piecePos) {
     final Pose3d cameraPos = new Pose3d(robotPos).transformBy(Constants.CAMERA_TO_ROBOT);
-    final Rotation3d invCameraRotation = cameraPos.getRotation().unaryMinus();
-    final Translation2d pieceTranslation = Constants.LIMELIGHT_HALF_POS.minus(piecePos);
-
+    final Vector<N3> cameraPosVec = cameraPos.getTranslation().toVector();
+    final Vector<N3> invCameraRotation = cameraPos.getRotation().unaryMinus().toVector();
+    final Vector<N2> pieceTranslation = Constants.LIMELIGHT_HALF_POS.minus(piecePos);
     final Vector<N3> pieceVec =
-        VecBuilder.fill(SPECIAL_Y, pieceTranslation.getX() * SPECIAL_RATIO, pieceTranslation.getY())
+        VecBuilder.fill(SPECIAL_Y, pieceTranslation.get(0) * SPECIAL_RATIO, pieceTranslation.get(1))
             .unit();
     double maxDot = -1.0;
     AlgaePositions closestAlgae = null;
-    for (AlgaePositions algae : ALGAES) {
+    for (int i = 0; i < ALGAES.length; i++) {
       Vector<N3> algaeVec =
-          algae
-              .getPosition()
-              .minus(cameraPos)
-              .getTranslation()
-              .rotateBy(invCameraRotation)
-              .toVector()
-              .unit();
+          Vector.cross(PRECOMPUTED_ALGAE_VECS[i].minus(cameraPosVec), invCameraRotation).unit();
       double dot = pieceVec.dot(algaeVec);
       if (dot > maxDot) {
         maxDot = dot;
-        closestAlgae = algae;
+        closestAlgae = ALGAES[i];
       }
     }
     return closestAlgae;
+  }
+
+  public static BranchPositions UnitTest1() {
+    double x = -4.1902085050000 / 3.8337244850000 * SPECIAL_X + Constants.LIMELIGHT_HALF_POS.get(0);
+    double y = -0.706978325 / 3.8337244850000 * SPECIAL_Y + Constants.LIMELIGHT_HALF_POS.get(1);
+    return FindCoralBranch(
+        new Pose2d(0, 0, new Rotation2d(0)), new Vector<N2>(VecBuilder.fill(x, y)));
   }
 }
--- src/main/java/frc/robot/commands/UnitTester.java
+++ src/main/java/frc/robot/commands/UnitTester.java
@@ -1,13 +1,10 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.utils.shuffleboard.SmartShuffleboard;
 
 public class UnitTester extends Command {
   @Override
-  public void execute() {
-  }
+  public void execute() {}
 
   @Override
   public boolean isFinished() {
