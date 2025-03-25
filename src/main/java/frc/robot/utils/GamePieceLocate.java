893d72d3e425434eebb133cb9fc25e1debefb644
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-03-25 16:19:33 -0400

    quaternions

--- src/main/java/frc/robot/utils/GamePieceLocate.java
+++ src/main/java/frc/robot/utils/GamePieceLocate.java
@@ -1,10 +1,8 @@
 package frc.robot.utils;
 
-import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.*;
-import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import frc.robot.constants.AlgaePositions;
 import frc.robot.constants.BranchPositions;
@@ -16,23 +14,23 @@ public class GamePieceLocate {
   private static final BranchPositions[] BRANCHES = BranchPositions.values();
   private static final AlgaePositions[] ALGAES = AlgaePositions.values();
   // Precomputed vectors for positions
-  private static final Vector<N3>[] PRECOMPUTED_BRANCH_VECS =
+  private static final Translation3d[] PRECOMPUTED_BRANCH_VECS =
       Arrays.stream(BRANCHES)
-          .map(branch -> branch.getPosition().getTranslation().toVector())
-          .toArray(Vector[]::new);
+          .map(branch -> branch.getPosition().getTranslation())
+          .toArray(Translation3d[]::new);
   ;
 
-  private static final Vector<N3>[] PRECOMPUTED_ALGAE_VECS =
+  private static final Translation3d[] PRECOMPUTED_ALGAE_VECS =
       Arrays.stream(ALGAES)
-          .map(algae -> algae.getPosition().getTranslation().toVector())
-          .toArray(Vector[]::new);
+          .map(algae -> algae.getPosition().getTranslation())
+          .toArray(Translation3d[]::new);
 
   // piece pos is in DEGREES, not RD
   public static BranchPositions findCoralBranch(
       Pose2d robotPos, double piecePosTXDeg, double piecePosTYDeg) {
     final Pose3d cameraPos = new Pose3d(robotPos).transformBy(Constants.LIMELIGHT_TO_ROBOT);
-    final Vector<N3> cameraPosVec = cameraPos.getTranslation().toVector();
-    final Matrix<N3, N3> invCameraRotation = cameraPos.getRotation().unaryMinus().toMatrix();
+    final Translation3d cameraPosVec = cameraPos.getTranslation();
+    final Rotation3d invCameraRotation = cameraPos.getRotation().unaryMinus();
     final Vector<N3> pieceVec =
         VecBuilder.fill(
                 1,
@@ -44,12 +42,13 @@ public class GamePieceLocate {
     int n = CenterPositions.getClosest(robotPos);
     for (int i = 6 * n - 3; i < 6 * n + 9; i++) {
       int f = Math.floorMod(i, BRANCHES.length);
-      Matrix<N3, N1> locationVec =
-          (invCameraRotation.times(PRECOMPUTED_BRANCH_VECS[f].minus(cameraPosVec)));
-      double dot =
-          pieceVec.dot(
-              VecBuilder.fill(locationVec.get(0, 0), locationVec.get(1, 0), locationVec.get(2, 0))
-                  .unit());
+      Vector<N3> locationVec =
+          PRECOMPUTED_BRANCH_VECS[f]
+              .minus(cameraPosVec)
+              .rotateBy(invCameraRotation)
+              .toVector()
+              .unit();
+      double dot = pieceVec.dot(locationVec);
       if (dot > maxDot) {
         maxDot = dot;
         closest = BRANCHES[f];
@@ -63,8 +62,8 @@ public class GamePieceLocate {
   public static AlgaePositions findAlgaePos(
       Pose2d robotPos, double piecePosTXDeg, double piecePosTYDeg) {
     final Pose3d cameraPos = new Pose3d(robotPos).transformBy(Constants.LIMELIGHT_TO_ROBOT);
-    final Vector<N3> cameraPosVec = cameraPos.getTranslation().toVector();
-    final Matrix<N3, N3> invCameraRotation = cameraPos.getRotation().unaryMinus().toMatrix();
+    final Translation3d cameraPosVec = cameraPos.getTranslation();
+    final Rotation3d invCameraRotation = cameraPos.getRotation().unaryMinus();
     final Vector<N3> pieceVec =
         VecBuilder.fill(
                 1,
@@ -76,8 +75,12 @@ public class GamePieceLocate {
     int n = CenterPositions.getClosest(robotPos);
     for (int i = 2 * n - 2; i < 2 * n + 4; i++) {
       int f = Math.floorMod(i, ALGAES.length);
-      Matrix<N3, N1> locationVec =
-          (invCameraRotation.times(PRECOMPUTED_ALGAE_VECS[f].minus(cameraPosVec)));
+      Vector<N3> locationVec =
+          PRECOMPUTED_ALGAE_VECS[f]
+              .minus(cameraPosVec)
+              .rotateBy(invCameraRotation)
+              .toVector()
+              .unit();
       double dot =
           pieceVec.dot(
               VecBuilder.fill(locationVec.get(0, 0), locationVec.get(1, 0), locationVec.get(2, 0))
