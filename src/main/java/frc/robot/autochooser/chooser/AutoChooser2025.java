a0d03cbff01a03c724dbdd734d23df52b73821e7
Jackson <wessjackson93@gmail.com>
2025-03-26 19:01:34 -0400

    added a command to switch to a robot centric drive mode (#284)
    * added a command to switch to a robot centric drive mode
    
    * update
    
    * update
    
    * added command to use it
    
    * restert
    
    * manual cross line
    
    * PR comments
    
    ---------
    
    Co-authored-by: codetoad <softwareenginer@pm.me>
    Co-authored-by: bengold22 <98672229+bengold22@users.noreply.github.com>

--- src/main/java/frc/robot/autochooser/chooser/AutoChooser2025.java
+++ src/main/java/frc/robot/autochooser/chooser/AutoChooser2025.java
@@ -9,25 +9,30 @@ import frc.robot.autochooser.event.AutoEvent;
 import frc.robot.autochooser.event.AutoEventProvider;
 import frc.robot.autochooser.event.AutoEventProviderIO;
 import frc.robot.commands.autos.*;
+import frc.robot.commands.drivetrain.RobotCentricDrive;
 import frc.robot.constants.ElevatorPosition;
 import frc.robot.subsystems.algaebyebyetilt.AlgaeByeByeTiltSubsystem;
 import frc.robot.subsystems.coral.CoralSubsystem;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.lightStrip.LightStrip;
+import frc.robot.subsystems.swervev3.SwerveDrivetrain;
 import frc.robot.utils.logging.commands.DoNothingCommand;
 import java.util.Map;
 
 public class AutoChooser2025 extends SubsystemBase implements AutoChooser {
   private final Map<AutoEvent, Command> commandMap;
   private final AutoEventProvider provider;
+  private final SwerveDrivetrain drivetrain;
 
   public AutoChooser2025(
       AutoEventProviderIO providerIO,
+      SwerveDrivetrain drivetrain,
       ElevatorSubsystem elevator,
       CoralSubsystem coral,
       LightStrip lightStrip,
       AlgaeByeByeTiltSubsystem byeBye) {
     provider = new AutoEventProvider(providerIO, this::isValid);
+    this.drivetrain = drivetrain;
     commandMap =
         Map.ofEntries(
             Map.entry(
@@ -38,13 +43,13 @@ public class AutoChooser2025 extends SubsystemBase implements AutoChooser {
                 new AutoEvent(AutoAction.DO_NOTHING, FieldLocation.RIGHT), new DoNothingCommand()),
             Map.entry(
                 new AutoEvent(AutoAction.CROSS_THE_LINE, FieldLocation.LEFT),
-                new LeftCrossTheLine(elevator, coral)),
+                new RobotCentricDrive(drivetrain, 0.25, 3)),
             Map.entry(
                 new AutoEvent(AutoAction.CROSS_THE_LINE, FieldLocation.MIDDLE),
-                new MiddleCrossTheLine(elevator, coral)),
+                new RobotCentricDrive(drivetrain, 0.25, 3)),
             Map.entry(
                 new AutoEvent(AutoAction.CROSS_THE_LINE, FieldLocation.RIGHT),
-                new RightCrossTheLine(elevator, coral)),
+                new RobotCentricDrive(drivetrain, 0.25, 3)),
             Map.entry(
                 new AutoEvent(AutoAction.TWO_PIECE_HIGH, FieldLocation.RIGHT),
                 new RightFourPieceFork(
